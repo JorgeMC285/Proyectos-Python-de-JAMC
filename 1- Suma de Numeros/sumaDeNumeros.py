1aae40356b3b26bdd54df6a4f4f42dfe55f066fe
JorgeMC285 <jorgemaciasc2008@gmail.com>
2026-03-09 19:11:07 -0600

    Suma de numeros

---
+++ 1- Suma de Numeros/sumaDeNumeros.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__": # Inicio
+    n1 = float(input("Ingrese el primer valor: "))
+    n2 = float(input("Ingrese el segundo valor: "))
+    res = n1 + n2
+    print(f"La suma de {n1:.2f} y {n2:.2f} es: {res:.2f}")
